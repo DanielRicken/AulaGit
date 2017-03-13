399534f3ba2f9a65ef816fd4d01dae24e66b157f
Daniel <Daniel@LAPTOP-SP2PAC3L>
2017-03-13 19:52:35 -0300

    Voltando ao trabalho na master.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		String word = "Word!";
-		String msg = "Hello" + word + "!";
+		String hello = "Hello";
+		String msg = hello + word + "!";
 		System.out.println(msg);
 		
 		
