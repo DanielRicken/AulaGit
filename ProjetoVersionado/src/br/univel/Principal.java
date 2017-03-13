e5d56269d830b4f267e45ad62b4761956bd556f4
Daniel <Daniel@LAPTOP-SP2PAC3L>
2017-03-13 19:48:05 -0300

    Alteração solicitada pelo cliente

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -5,7 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		String msg = "Hello Word!";
+		System.out.println("---------");
 		System.out.println(msg);
+		System.out.println("---------");
 		
 		
 	}
