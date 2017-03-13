21269d2ce2a125912d8d9389ab7eb9d41bf04297
Daniel <Daniel@LAPTOP-SP2PAC3L>
2017-03-13 19:16:37 -0300

    Alteração após a criação da tag.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		String msg = "Hello Word!";
+		String word = "Word!";
+		String msg = "Hello" + word + "!";
 		System.out.println(msg);
 		
 		
